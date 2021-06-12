4b273080d41de4db924109213ef76fd818f4912c
Misha <misha.arzumanov@yandex.ru>
2021-06-12 16:36:26 +0300

    Default map was added

--- read_path.cpp
+++ read_path.cpp
@@ -2,8 +2,10 @@
 #include "Environment.hpp"
 
 void read_path() {
-	std::cout << "Input the path to the map: ";
-	std::cin >> map_path;
+	std::cout << "Input the path to the map (empty for default): ";
+	std::getline(std::cin, map_path);
+	if (map_path == "")
+		map_path = "example_1.map";
 	while (read_the_map(map_path)) {
 		std::cout << "File doesn't exist, reenter the path to the map: ";
 		std::cin >> map_path;
